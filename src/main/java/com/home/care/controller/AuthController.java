0241b8570061d3c163613b3f41bffe0a2353ff40
Bhabesh <bmukhopa@sfu.ca>
2022-11-12 14:16:25 -0800

    Password encode

--- src/main/java/com/home/care/controller/AuthController.java
+++ src/main/java/com/home/care/controller/AuthController.java
@@ -3,23 +3,19 @@
  */
 package com.home.care.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.home.care.bo.User;
-import com.home.care.db.UserRepo;
 import com.home.care.login.LoginService;
 import com.home.care.login.LoginUser;
-import com.home.care.service.UserService;
-
-import net.bytebuddy.dynamic.scaffold.TypeWriter.FieldPool.Record;
 
 /**
  * @author bhabesh
@@ -29,15 +25,17 @@ import net.bytebuddy.dynamic.scaffold.TypeWriter.FieldPool.Record;
 @RequestMapping(value="/api")
 public class AuthController {
 	@Autowired
-	UserService userService;
-	@Autowired
 	LoginService loginService;
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
 	@PostMapping(value = "/register", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
 	public LoginUser register(@RequestBody LoginUser user) {
 		if (!user.getPassword().equals(user.getPasswordCconfirm())) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password did not match");
 		}
+		
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		return loginService.register(user);
 	}
 }
---
+++ src/main/java/com/home/care/login/BeanConfig.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.home.care.login;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author bhabesh
+ *
+ */
+@Configuration
+public class BeanConfig {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
